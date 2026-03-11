7559857052401e04e8e3dae66e7f710b8bb6b990
ronnymunzhedzi <ronewamunzhedzi85@gmail.com>
2026-03-11 14:34:41 +0200

    about too add file reading and word cleaning

--- Anagrams.java
+++ Anagrams.java
@@ -1,1 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class Anagrams{
+  private static HashMap<String , ArrayList<String>>anagramMap = new HashMap<>();
+  private static int totalWords = 0;
+  private static int uniqueWords= 0;
+  private static int anagramGroups =0;
+  public static void main(String[]args) {
+    Sysytem.out.println("====ANAGRAM FINDER FOR joyce1922_ulysses.txt ====");
+    Sysytem.out.println("Starting at : " + new Date());
+    Sysytem.out.println("-----------------------------------------")
+      if (!processFile(" joyce1922_ulysses.txt")) {
+            System.out.println("ERROR: Could not process file. Exiting.");
+            return;
+        }
+    printStatistics();
+        generateLatexFile("theAnagrams.tex");
+        System.out.println("\nDone! Output written to theAnagrams.tex");
+    }
+  
 
+
+  
